6b0f7b64ddd12b3bc6fb29bb0048731daf013f80
YvetteFC <yvettefloresc@icloud.com>
2023-11-03 16:27:08 -0500

    Initial commit

---
+++ src/main/java/com/ykeas/ykeasmsprincipal/YkeasMsPrincipalApplication.java
@@ -0,0 +1,13 @@
+package com.ykeas.ykeasmsprincipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkeasMsPrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YkeasMsPrincipalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykeas/ykeasmsprincipal/YkeasMsPrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykeas.ykeasmsprincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YkeasMsPrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
